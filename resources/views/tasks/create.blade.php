4a30624a54d76530282bb67852346377b3819815
y.m <sunny_yoko45@yahoo.co.jp>
2018-11-12 06:59:50 +0000

    bootstrap kadaitask

--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -4,16 +4,27 @@
 
     <h1>新規タスク作成ページ</h1>
     
-    {!! Form::model($task, ['route' => 'tasks.store']) !!}
-    
-        {!! Form::label('status','ステータス:') !!}
-        {!! Form::text('status') !!}
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-12 col-sm-offset-2 col-sm-8 col-md-offset-2 col-md-8 col-lg-offset-3 col-lg-6">
         
-        {!! Form::label('content','タスク:') !!}
-        {!! Form::text('content') !!}
+                {!! Form::model($task, ['route' => 'tasks.store']) !!}
+                
+                    <div class="form-group">
+                        {!! Form::label('status','ステータス:') !!}
+                        {!! Form::text('status', null, ['class' => 'form-control']) !!}
+                    </div>
+                    
+                    <div class="form-group">
+                        {!! Form::label('content','タスク:') !!}
+                        {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                    </div>
+                    
+                    {!! Form::submit('タスク登録', ['class' => 'btn btn-primary']) !!}
         
-        {!! Form::submit('タスク登録') !!}
-    
-    {!! Form::close() !!}
+                {!! Form::close() !!}
+            </div>
+        </div>
+    </div>
 
 @endsection
